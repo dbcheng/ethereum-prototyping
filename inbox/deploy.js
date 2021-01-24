c08ca37e363b082bda5ab890a4fac23e8a5dcf91
David Cheng <dbcheng@google.com>
2021-01-23 20:02:00 -0800

    Inbox Tests and Deployments

---
+++ inbox/deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+	'knife decrease goddess argue winter company rescue breeze siege captain name interest',
+	'https://rinkeby.infura.io/v3/11c8a5c12e7041468d0f01e7cf3f3c71'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+	console.log('Attempting to deploy from account ', accounts[0]);
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi there'] })
+		.send({ gas: '1000000', from: accounts[0] });
+	console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,4 +1,35 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
 
+let accounts;
+let inbox;
+const INITIAL_STRING = 'hi there';
+
+beforeEach(async () => {
+    // Get a list of accounts
+    accounts = await web3.eth.getAccounts();
+    // use one account for contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+        .send({ from: accounts[0], gas: '1000000' });
+    inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+    it('has initial message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+    it('change msg', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
+    });
+});
